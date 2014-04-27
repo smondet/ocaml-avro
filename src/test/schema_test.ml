8c72da1765c635907e9ebd6c37307357bd203c20
Sebastien Mondet <seb@mondet.org>
2014-04-27 17:42:55 -0400

    test: add tests up to non-implemented parsing

--- src/test/schema_test.ml
+++ src/test/schema_test.ml
@@ -9,14 +9,22 @@ let test_failures : string list ref = ref []
 let fail_test s = test_failures := s :: !test_failures
 let fail_testf fmt = Printf.ksprintf fail_test fmt
 
-let test_ok ~json expected =
+let string_test_ok ~json expected =
   begin match Avro_schema.of_json (json_of_string json) with
   | `Ok o -> if o = expected then () else fail_test json
   | `Error e ->
     fail_testf "%s → Error %s" json 
       (Avro_schema.error_to_string ~json_to_string e)
   end
-let test_error ~json expected =
+let json_test_ok ~json expected =
+  begin match Avro_schema.of_json json with
+  | `Ok o -> if o = expected then () else fail_test (json_to_string json)
+  | `Error e ->
+    fail_testf "%s → Error %s" (json_to_string json)
+      (Avro_schema.error_to_string ~json_to_string e)
+  end
+
+let string_test_error ~json expected =
   begin match Avro_schema.of_json (json_of_string json) with
   | `Ok o -> fail_testf "Got OK: %s" json
   | `Error e when e = expected -> ()
@@ -24,12 +32,102 @@ let test_error ~json expected =
     fail_testf "%s → Error %s" json 
       (Avro_schema.error_to_string ~json_to_string e)
   end
+let json_test_error ~json expected =
+  begin match Avro_schema.of_json json with
+  | `Ok o -> fail_testf "Got OK: %s" (json_to_string json)
+  | `Error e when e = expected -> ()
+  | `Error e ->
+    fail_testf "%s → Error %s" (json_to_string json) 
+      (Avro_schema.error_to_string ~json_to_string e)
+  end
+
+module Cons = struct
+  (* A curated version should go to the library itself *)
+
+  open Avro_schema
+
+  let record ~name ~namespace ?doc ?(aliases=[]) fields =
+    `Record {record_name = name;
+             record_namespace = namespace;
+             record_doc = doc;
+             record_aliases = aliases;
+             record_fields = fields;}
+
+  let field ?doc ?default_value ?order ?(aliases=[]) (name, avro_type) =
+    {
+      field_name = name;
+      field_doc = doc;
+      field_type = avro_type;
+      field_default = default_value;
+      field_order = order;
+      field_aliases = aliases;
+    }
+
+end
 
 let () =
-  test_ok ~json:"\"some_json_string\"" (`Named_type "some_json_string");
-  test_error ~json:"\"0some_json_string\"" (`Invalid_avro_name "0some_json_string");
-  test_error ~json:"0" (`Unexpected_json ("toplevel", `Int 0));
-  test_ok ~json:"[\"one\", \"int\"]" (`Union [`Named_type "one"; `Int]);
+  (* Some “whole parsings” tests, string -> Avro *) 
+  string_test_ok ~json:"\"some_json_string\"" (`Named_type "some_json_string");
+  string_test_error ~json:"\"0some_json_string\"" (`Invalid_avro_name "0some_json_string");
+  string_test_error ~json:"0" (`Unexpected_json ("toplevel", `Int 0));
+  string_test_ok ~json:"[\"one\", \"int\"]" (`Union [`Named_type "one"; `Int]);
+  string_test_ok ~json:"{ \"type\": \"someName\" }" 
+    (`Object (`Named_type "someName", []));
+  (* Now with JSON AST as input (i.e. the actual parsing done by the library) *)
+  let assoc l = (`Assoc l : Yojson.Basic.json) in
+  (* `json_test_ok` checks some Yojson.Basic.json against some Avro schema: *)
+  json_test_ok ~json:(assoc ["type", `String "someName"; "attr_one", `String "attr_one_value"])
+    (`Object (`Named_type "someName", ["attr_one", `String "attr_one_value"]));
+  let json_record ?name ?namespace ?doc ?aliases more_attr =
+    (* Build potentially erroneous JSON for an Avro record *)
+    let string_field k v = (k, `String v) in 
+    let array_of_strings_field k v =
+      (k, `List (List.map (fun s -> `String s) v)) in
+    let add_option opt f l =
+      match opt with
+      | Some n -> f n :: l
+      | None -> l in
+    let actual_attr =
+      more_attr
+      |> add_option namespace (string_field "namespace")
+      |> add_option name (string_field "name")
+      |> add_option doc (string_field "doc")
+      |> add_option aliases (array_of_strings_field "aliases")
+    in
+    assoc (("type", `String "record") :: actual_attr)
+  in
+  (* Test records without fields: *)
+  let name = "SomeName" in
+  let namespace = "SomeNamespace" in
+  json_test_ok ~json:(json_record ~name ~namespace [])
+    (Cons.record ~name ~namespace []);
+  json_test_ok ~json:(json_record ~name ~namespace ~doc:"DOC" [])
+    (Cons.record ~name ~namespace ~doc:"DOC" []);
+  json_test_ok ~json:(json_record ~name ~namespace ~doc:"DOC" ~aliases:["bouh"; "bah"] [])
+    (Cons.record ~name ~namespace ~doc:"DOC" ~aliases:["bouh"; "bah"] []);
+  (* Records without fields but errors: *)
+  let wrong_json_for_aliases = `String "someString" in
+  json_test_error ~json:(
+    json_record ~name ~namespace ~doc:"DOC" ["aliases", wrong_json_for_aliases])
+    (`Unexpected_json ("record.aliases", wrong_json_for_aliases));
+  let wrong_json_for_doc = `List [`String "someString"] in
+  json_test_error ~json:(
+    json_record ~name ~namespace ["doc", wrong_json_for_doc])
+    (`Unexpected_json ("record.doc", wrong_json_for_doc));
+  (* Now to test fields, let's get some decent JSON and its corresponding avro
+     as basis: *)
+  let good_json_record fields =
+    json_record ~name ~namespace ~doc:"DOC" ~aliases:["bouh"; "bah"] fields in
+  let good_record_parsed fields =
+    Cons.record ~name ~namespace ~doc:"DOC" ~aliases:["bouh"; "bah"] fields in
+  json_test_ok (good_json_record []) (good_record_parsed []);
+  json_test_ok 
+    (good_json_record ["fields", `List []])
+    (good_record_parsed []);
+  json_test_ok 
+    (good_json_record ["fields", `List [`Assoc ["name", `String "FieldName1"; "type", `String "string"]]])
+    (good_record_parsed []);
+
   begin match test_failures with
   | {contents = []} -> print_string "Tests OK\n"
   | {contents} ->
